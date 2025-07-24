c7b62225360c110876a840f2023f3ce98872e873
= <shubhamagrawal290304@gmail.com>
2025-07-24 13:32:48 +0530

    blogAPP

---
+++ BlogApp/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+
+  console.log(import.meta.env.VITE_APPWRITE_URL);
+  
+
+
+  return (
+    <>
+    <h1>A Blog writting app</h1>
+    </>
+  )
+}
+
+export default App
