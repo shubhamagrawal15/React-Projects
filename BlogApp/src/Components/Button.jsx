3bfbe8bd2d1a8e5ac562a63702ef5ebee81d0722
= <shubhamagrawal290304@gmail.com>
2025-07-31 10:49:42 +0530

    components

---
+++ BlogApp/src/Components/Button.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Button({
+  children,                          // or btntext 
+  type='button',
+  bgColor = 'bg-blue-600',
+  textColor ='text-white',
+  className ='',
+  ...props
+}){
+  return (
+    <Button className={`px-4 py-2 rounded-lg ${bgColor} ${textColor} ${className}`} {...props}
+    >{children}</Button>
+  )
+}
+
+export default Button
